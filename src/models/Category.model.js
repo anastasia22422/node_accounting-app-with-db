683b8bb26a2fab08aef23ac4cc30bc864146c22e
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-08-06 21:28:27 +0200

    Add files via upload

---
+++ src/models/Category.model.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db.js');
+
+const Category = sequelize.define(
+  'Category',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    tableName: 'categories',
+    timestamps: false,
+  },
+);
+
+module.exports = { Category };
